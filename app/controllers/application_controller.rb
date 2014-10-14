048d3fa1400536f0c57ddc0f63419e41adc629de
Mr.Rene <venelrene@gmail.com>
2014-10-13 21:30:59 -0400

    assignemt#28

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,23 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+require 'json'
+require 'net/http'
+
+  def index
+  end
+
+  def movie_query
+
+  movie = params[:title]
+  uri = URI("http://www.myapifilms.com/imdb?title=" + movie)
+
+
+    response = Net::HTTP.get(uri)
+
+    @body = JSON.parse(response).first
+
+  end
+
+end
